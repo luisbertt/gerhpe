d7da3cd976fe290de431921452e44f828709add1
luisbertt <luisbert2506@gmail.com>
2021-08-18 06:39:45 -0400

    Comment Out More Code

--- src/App.js
+++ src/App.js
@@ -22,53 +22,53 @@ const App = () => {
     )
 }
 
-const PaintingsContainer = () => {
-    const [paintings, setPaintings] = useState([])
+// const PaintingsContainer = () => {
+//     const [paintings, setPaintings] = useState([])
 
-    useEffect(() => {
-        async function getData() {
-            const data = await getLatestPaintings(25)
-            const grid = new Array(3)
-                .fill()
-                .map((_, i) => data.filter((_, j) => j % 3 === i))
-            setPaintings(grid)
-        }
-        getData()
-    }, [])
+//     useEffect(() => {
+//         async function getData() {
+//             const data = await getLatestPaintings(25)
+//             const grid = new Array(3)
+//                 .fill()
+//                 .map((_, i) => data.filter((_, j) => j % 3 === i))
+//             setPaintings(grid)
+//         }
+//         getData()
+//     }, [])
 
-    return (
-        <div className="container mx-auto mt-4 space-y-4">
-            <div className="text-center mt-4">
-                <img
-                    src={GerhpeLogo}
-                    alt="Gerhpe Logo"
-                    className="w-96 mx-auto border rounded"
-                />
-                <h1 className="text-3xl font-bold text-center mt-4">
-                    Gerhpe's Gallery
-                </h1>
-                <p className="text-lg text-gray-500 italic">
-                    If you have a Gerhpe, you have a good taste...
-                </p>
-            </div>
-            <div className="flex flex-wrap space-x-4 justify-center">
-                {paintings.length
-                    ? paintings.map((col, i) => (
-                          <div className="w-1/4 space-y-4" key={i}>
-                              {col.map(painting => (
-                                  <Link
-                                      to={`paintings/${painting.id}`}
-                                      key={painting.id}
-                                  >
-                                      <ArtworkCard painting={painting} />
-                                  </Link>
-                              ))}
-                          </div>
-                      ))
-                    : null}
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div className="container mx-auto mt-4 space-y-4">
+//             <div className="text-center mt-4">
+//                 <img
+//                     src={GerhpeLogo}
+//                     alt="Gerhpe Logo"
+//                     className="w-96 mx-auto border rounded"
+//                 />
+//                 <h1 className="text-3xl font-bold text-center mt-4">
+//                     Gerhpe's Gallery
+//                 </h1>
+//                 <p className="text-lg text-gray-500 italic">
+//                     If you have a Gerhpe, you have a good taste...
+//                 </p>
+//             </div>
+//             <div className="flex flex-wrap space-x-4 justify-center">
+//                 {paintings.length
+//                     ? paintings.map((col, i) => (
+//                           <div className="w-1/4 space-y-4" key={i}>
+//                               {col.map(painting => (
+//                                   <Link
+//                                       to={`paintings/${painting.id}`}
+//                                       key={painting.id}
+//                                   >
+//                                       <ArtworkCard painting={painting} />
+//                                   </Link>
+//                               ))}
+//                           </div>
+//                       ))
+//                     : null}
+//             </div>
+//         </div>
+//     )
+// }
 
 export default App
