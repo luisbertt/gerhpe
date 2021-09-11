cfd0e05766a50561c3bad0e0559210afcb4af3ed
luisbertt <luisbert2506@gmail.com>
2021-09-11 16:01:59 -0400

    Updated Artwork Page

--- src/components/ArtworkPage.jsx
+++ src/components/ArtworkPage.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 import ArtworkPageSkeleton from "./ArtworkPageSkeleton"
 import { getPainting } from "../api"
+import Gerhpe from "../data/gerhpe_logo.jpg"
 
 const ArtworkPage = ({ match }) => {
     const [painting, setPainting] = useState(null)
@@ -15,23 +17,47 @@ const ArtworkPage = ({ match }) => {
 
     if (painting)
         return (
-            <div className="container mx-auto mt-8 flex border p-4 space-x-4">
-                <div className="">
-                    <img
-                        src={painting.Photo[0].thumbnails.large.url}
-                        alt={painting.Name}
-                        className="h-96"
-                    />
-                </div>
-                <div className="">
-                    <h1 className="text-3xl font-bold">{painting.Name}</h1>
-                     <p>Size: {painting.Size}</p>
-                     <p>Type: {painting.Type}</p>
-                     <p>Author: {painting.Author}</p>
-                     <p>Status: {painting.Status}</p>
-                     <p>Line: {painting.Line}</p>
+            <>
+            <div className="container mx-auto"> 
+            <div className="flex space-x-20 h-full">
+                <Link to="/Home"><img src={Gerhpe} alt="" width="250px"/></Link>
+                <ul className="flex space-x-10 text-2xl m-auto">
+                    <Link to="/">Home</Link>
+                    <Link to="/">Gallery</Link>
+                    <Link to="/">Future Events</Link>
+                    <Link to="/">Contact</Link>
+                </ul>
+            </div>
+                <div className=" flex space-x-10">
+                  
+                        <div className="border p-4">
+                           
+                                <img
+                                    src={painting.Photo[0].thumbnails.large.url}
+                                    alt={painting.Name}
+                                    className="mx-auto"
+                                />
+                            
+                        </div>
+                    
+                    <div className="flex flex-col space-y-5">
+                        <h1 className="text-5xl font-bold">{painting.Name}</h1>
+                         <div className="">
+                            <p>Size: {painting.Size}</p>
+                            <p>Type: {painting.Type}</p>
+                            <p>Author: {painting.Author}</p>
+                            <p>Status: {painting.Status}</p>
+                            <p>Line: {painting.Line}</p>
+                        </div>
+                        <div>
+                            
+                            <a href={painting.Photo[1].thumbnails.large.url} target="_blank" rel="noreferrer"><button className="text-xl border p-2 rounded">Set Preview</button></a>
+                            {/* <img src={painting.Photo[1].thumbnails.large.url} alt={painting.Name + ' Ambientado'} className="mt-2"/> */}
+                        </div>
+                    </div>
                 </div>
             </div>
+            </>
         )
     return <ArtworkPageSkeleton />
 }
