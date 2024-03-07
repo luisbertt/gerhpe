a1d086327557af290671d6e9c355d261c7235573
Luisbert Seijas <luisbrttt@gmail.com>
2024-03-06 21:57:33 -0700

    ok, now it should work

--- pages/api/paintings/[id].js
+++ pages/api/paintings/[id].js
@@ -1,6 +1,6 @@
 const Airtable = require("airtable")
-const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env
-const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID)
+const { AIRTABLE_API_TOKEN, AIRTABLE_BASE_ID } = process.env
+const base = new Airtable({ apiKey: AIRTABLE_API_TOKEN }).base(AIRTABLE_BASE_ID)
 const table = base("Paintings")
 
 export default async (req, res) => {
